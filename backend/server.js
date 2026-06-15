b63367e6b65c3b65a24b414d2dcaa82eeb65b0dc
m-alladi2006 <alladimanjunadh07@gmail.com>
2026-06-15 12:22:03 +0530

    Connect Stock Routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const connectDB = require("./config/db");
 const authRoutes =
 require("./routes/authRoutes");
 
+const stockRoutes = require("./routes/stockRoutes");
+
 const app = express();
 
 app.use(express.json());
@@ -14,6 +16,8 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/auth",authRoutes);
+
+app.use("/api/stocks", stockRoutes);
 
 app.get("/",(req,res)=>{
     res.send("API Running");
